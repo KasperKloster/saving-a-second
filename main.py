5e6a0a058bdf8c7ee35f322cadf7884b8d95e31f
Kasper Kloster <32112114+KasperKloster@users.noreply.github.com>
2020-12-04 23:58:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,5 @@ choice = raw_input('Which project will you work on?')
 os.system("mysql.server start")
 # Open Browser
 webbrowser.open('http://127.0.0.1:8000')
-# Navigate to folder which user choices.
-# Run php artisan serve
+# Navigate to folder which user choices and Run php artisan serve
 os.system( "cd " + path + projectFolders[int(choice)] + " && php artisan serve" )
